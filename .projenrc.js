fa4758eb70891a80df505b9563603ca866d0cf21
Pahud Hsieh <pahudnet@gmail.com>
2021-06-08 08:11:05 +0800

    bump projen (#256)

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-const { AwsCdkConstructLibrary } = require('projen');
+const { AwsCdkConstructLibrary, DependenciesUpgradeMechanism } = require('projen');
 
 const AWS_CDK_LATEST_RELEASE = '1.79.0';
 const PROJECT_NAME = 'cdk-fargate-express';
@@ -11,8 +11,16 @@ const project = new AwsCdkConstructLibrary({
   repository: 'https://github.com/pahud/cdk-fargate-express.git',
   authorName: 'Pahud Hsieh',
   authorEmail: 'pahudnet@gmail.com',
-  stability: 'experimental',
-  dependabot: false,
+  depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      secret: AUTOMATION_TOKEN,
+    },
+  }),
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['pahud', 'cdk-automation'],
+  },
   keywords: [
     'aws',
     'cdk',
@@ -23,11 +31,7 @@ const project = new AwsCdkConstructLibrary({
     twitter: 'pahudnet',
     announce: false,
   },
-  defaultReleaseBranch: ['master'],
-
-  // creates PRs for projen upgrades
-  // projenUpgradeSecret: 'AUTOMATION_GITHUB_TOKEN',
-
+  defaultReleaseBranch: 'main',
   cdkVersion: AWS_CDK_LATEST_RELEASE,
   cdkDependencies: [
     '@aws-cdk/core',
@@ -35,53 +39,11 @@ const project = new AwsCdkConstructLibrary({
     '@aws-cdk/aws-ecs',
     '@aws-cdk/aws-ecs-patterns',
   ],
-
   python: {
     distName: 'cdk-fargate-express',
     module: 'cd_fargate_express',
   },
 });
-
-// create a custom projen and yarn upgrade workflow
-workflow = project.github.addWorkflow('ProjenYarnUpgrade');
-
-workflow.on({
-  schedule: [{
-    cron: '11 0 * * *',
-  }], // 0:11am every day
-  workflow_dispatch: {}, // allow manual triggering
-});
-
-workflow.addJobs({
-  upgrade: {
-    'runs-on': 'ubuntu-latest',
-    'steps': [
-      { uses: 'actions/checkout@v2' },
-      {
-        uses: 'actions/setup-node@v1',
-        with: {
-          'node-version': '10.17.0',
-        },
-      },
-      { run: 'yarn upgrade' },
-      { run: 'yarn projen:upgrade' },
-      // submit a PR
-      {
-        name: 'Create Pull Request',
-        uses: 'peter-evans/create-pull-request@v3',
-        with: {
-          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
-          'commit-message': 'chore: upgrade projen',
-          'branch': 'auto/projen-upgrade',
-          'title': 'chore: upgrade projen and yarn',
-          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
-          'labels': 'auto-merge',
-        },
-      },
-    ],
-  },
-});
-
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log'];
 project.npmignore.exclude(...common_exclude);
